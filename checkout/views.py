d2fcca16f10196c56f81d45abe0db2b8e27e2730
kreilly86 <kirstyrebeccareilly@gmail.com>
2022-07-04 15:28:14 +0000

    Add basic checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -31,7 +31,7 @@ def checkout(request):
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
              Did you forget to set it in your environment?')
-            
+          
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
