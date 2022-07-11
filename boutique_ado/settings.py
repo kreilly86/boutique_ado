a968dda789bc372d740a5b473c185aa606f4afed
kreilly86 <kirstyrebeccareilly@gmail.com>
2022-07-11 19:35:27 +0000

    Update checkout form

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
+SECRET_KEY = 'django-insecure-z0(!yb)ku*e4!eqpvf1obt7qgo!fd8+&yp=r@r+(y84$+53g-)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
