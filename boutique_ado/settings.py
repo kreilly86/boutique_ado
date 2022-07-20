cd482410f36019fee08ba1117bbd89c276c7f040
kreilly86 <kirstyrebeccareilly@gmail.com>
2022-07-20 20:59:51 +0000

    Update bucket name

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -180,7 +180,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'boutique-adp'
+    AWS_STORAGE_BUCKET_NAME = 'arn:aws:s3:::boutique-adp'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
